26ee490b77b2f78acfdfa2b7d80916fdae97ae44
Felix Endres <f.endres@proton.me>
2023-11-08 08:25:54 +0100

    Lektion5

---
+++ Uebungen/Lektion5/UB1.java
@@ -0,0 +1,21 @@
+package Lektion5;
+
+public class UB1 {
+    public static void main(String[] args) {
+        int k = 1;
+        double sum = 0;
+        double prevSum;
+        double diff;
+
+        do {
+            prevSum = sum;
+            sum += 1.0 / (k * k);
+            diff = sum - prevSum;
+            if (diff < 0) {
+                diff = -diff;
+            }
+            k++;
+            System.out.println(6*sum);
+        } while (diff >= 1e-5);
+    }
+}
---
+++ Uebungen/Lektion5/UB6.java
@@ -0,0 +1,24 @@
+package Lektion5;
+import java.util.Scanner;
+public class UB6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ihre Eingaben werden addiert.");
+        System.out.println("Die Eingabe von 0 beendet das Programm und gibt die Summe aus.");
+
+        while(true) {
+            num = sc.nextInt();
+            if (num == 0) {
+                System.out.println(sum);
+                break;
+            } else
+                sum += num;
+
+
+        }
+    }
+}
